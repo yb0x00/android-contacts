6b3e1ec0563530546cc635392aafff9ab576d3a1
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-26 02:30:30 +0900

    feat: Add showMoreEditTexts function to hide button and show layout on click
    더보기 버튼을 클릭하면,
    - 더보기 버튼이 사라지고
    - 생일, 성별, 메모 입력란 나타남

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -2,19 +2,38 @@ package campus.tech.kakao.contacts
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var cancel: Button
     lateinit var save: Button
+    lateinit var more: Button
+    lateinit var more_layout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        showMoreEditTexts()
         mydebug()
+    }
+
+    fun showMoreEditTexts(){
+        more = findViewById(R.id.btn_more)
+        more_layout = findViewById(R.id.field_2)
+
+        more.setOnClickListener {
+            more.visibility = if(more.visibility == View.VISIBLE){
+                View.GONE
+            } else {View.VISIBLE}
+            more_layout.visibility = if(more_layout.visibility == View.GONE){
+                View.VISIBLE
+            } else {View.GONE}
+        }
     }
 
     fun mydebug(){
