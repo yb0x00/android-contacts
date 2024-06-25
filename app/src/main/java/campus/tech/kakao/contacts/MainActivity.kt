c0e1fbf99d552d0cd48d748d7b604152f521dfef
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-25 17:50:56 +0900

    feat: Add mydebug() function
    UI 테스트 함수

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,32 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cancel: Button
+    lateinit var save: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mydebug()
+    }
+
+    fun mydebug(){
+        cancel = findViewById(R.id.btn_cancel)
+        save = findViewById(R.id.btn_save)
+
+        cancel.setOnClickListener {
+            Log.d("버튼","취소");
+        }
+
+        save.setOnClickListener {
+            Log.d("버튼","저장");
+        }
     }
 }
