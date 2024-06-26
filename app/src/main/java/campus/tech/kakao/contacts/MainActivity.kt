ad3a93d4216d1b9d9475fb82a6902cd519bf663a
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-26 15:14:08 +0900

    feat: Add showDatePicker function using date picker for birth date input
    - 생일 입력란을 클릭하면 달력이 뜨고,
    - 달력에서 날짜를 선택한 후 확인을 누르면 날짜가 입력됨

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,9 +1,12 @@
 package campus.tech.kakao.contacts
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,12 +16,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var save: Button
     lateinit var more: Button
     lateinit var more_layout: LinearLayout
+    lateinit var date_birth : EditText
+    lateinit var dateString : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showMoreEditTexts()
+        showDatePicker()
         mydebug()
     }
 
@@ -33,6 +39,20 @@ class MainActivity : AppCompatActivity() {
             more_layout.visibility = if(more_layout.visibility == View.GONE){
                 View.VISIBLE
             } else {View.GONE}
+        }
+    }
+
+    fun showDatePicker(){
+        date_birth = findViewById(R.id.edit_birth)
+
+        date_birth.setOnClickListener {
+            val cal = Calendar.getInstance()
+
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                dateString= "${year}.${month+1}.${dayOfMonth}"
+                date_birth.setText(dateString)
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 
