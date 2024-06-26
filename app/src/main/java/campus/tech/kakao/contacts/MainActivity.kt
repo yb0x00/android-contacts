f821935b0c8a5423afd2f173b8ca6a65beac583b
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-26 16:38:03 +0900

    feat: Hide keyboard when date picker is shown
    - setUI()에서 InputMethodManager 초기화
    - showDatePicker()에서 기능 구현

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,10 +1,13 @@
 package campus.tech.kakao.contacts
 
 import android.app.DatePickerDialog
+import android.content.Context
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
@@ -16,8 +19,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var save: Button
     lateinit var more: Button
     lateinit var more_layout: LinearLayout
+
     lateinit var date_birth : EditText
     lateinit var dateString : String
+    lateinit var imm : InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +37,10 @@ class MainActivity : AppCompatActivity() {
     fun setUI(){
         more = findViewById(R.id.btn_more)
         more_layout = findViewById(R.id.field_2)
+
         date_birth = findViewById(R.id.edit_birth)
+        imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         cancel = findViewById(R.id.btn_cancel)
         save = findViewById(R.id.btn_save)
     }
@@ -51,6 +59,11 @@ class MainActivity : AppCompatActivity() {
     fun showDatePicker(){
         date_birth.setOnClickListener {
             val cal = Calendar.getInstance()
+
+            imm.hideSoftInputFromWindow(
+                currentFocus?.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
 
             val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 dateString= "${year}.${month+1}.${dayOfMonth}"
