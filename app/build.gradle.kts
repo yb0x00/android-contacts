8c9d7fd96226bd931423eac4dd40ff02c521d644
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-26 14:37:36 +0900

    Revert "refactor: Change EditText hint"
    This reverts commit a27a691a5347cd0718241f5339bd7be56cbb25b4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    buildFeatures {
-        viewBinding=true
-    }
 }
 
 dependencies {
@@ -45,7 +41,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.databinding:library:3.2.0-alpha11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,18 +1,11 @@
 package campus.tech.kakao.contacts
 
-import android.app.DatePickerDialog
-import android.icu.util.Calendar
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.DatePicker
-import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import campus.tech.kakao.contacts.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +13,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var save: Button
     lateinit var more: Button
     lateinit var more_layout: LinearLayout
-    lateinit var edittext_date : EditText
-    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showMoreEditTexts()
-        calendar()
         mydebug()
     }
 
@@ -43,20 +33,6 @@ class MainActivity : AppCompatActivity() {
             more_layout.visibility = if(more_layout.visibility == View.GONE){
                 View.VISIBLE
             } else {View.GONE}
-        }
-    }
-
-    fun calendar(){
-        edittext_date = findViewById(R.id.edit_birth)
-
-        edittext_date.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val data = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                binding.editBirth.hint = "${year}.${month+1}.${dayOfMonth}"
-            }
-            DatePickerDialog(this, data, cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 
